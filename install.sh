16f0248eb21a52268d1a7b43f422aaffab9040ea
tony baldwin <tony@tonybaldwin.org>
2013-05-04 18:09:32 -0400

    first commit

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+##############################################################
+# installation script for runlog                             #
+# runners workout log management in bash by tony baldwin     #
+# http://tonyb.us/runlog                                     #
+# released according to the terms of the GNU GPL v.3 or later#
+##############################################################
+
+if [ != "$HOME/bin/" ]; then
+	mkdir $HOME/bin/
+	$PATH=$PATH:/$HOME/bin/
+	export PATH
+fi
+
+rlpath="$HOME/.runlog/"
+editor="/usr/bin/vim"
+
+echo "installing runlog ... "
+cp runlog.sh $HOME/bin/runlog
+chmod +x $HOME/bin/runlog
+
+echo "Creating config files ... "
+read -p "Where do you wish to keep your runlog files? (default ~/.runlog/ If you choose another directory, do not forget trailing slash.): " rlpath
+read -p "What is your prefered editor? (default /usr/bin/vim): " editor
+
+echo "# runlog config " > $HOME/.runlog.conf
+echo "rlpath=$rlpath" >> $HOME/.runlog.conf
+echo "editor=$editor" >> $HOME/.runlog.conf
+
+echo "Installation complete."
+
+runlog h
+exit
---
+++ runlog.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+#################################
+# runlog - by tony baldwin      #
+# http://tonyb.us/runlog        #
+# keeping a personal runlog     #
+# released according to GPL v.3 #
+#################################
+
+filedate=$(date +%Y%m%d%H%M%S)
+thismonth=$(date +%Y%m)
+tday=$(date +%Y%m%d)
+
+
+source  ~/.runlog.conf
+cd $rlpath
+
+if [[ $1 = r ]]; then
+	if [[ $2 ]]; then
+		cat $2* | less
+	else
+		for i in $(ls -Rt *.run); do cat $i; done | less
+	fi
+else
+if [[ $1 = h ]]; then
+	echo "runlog management system by tony baldwin, http://tonyb.us/runlog
+----------- runlog usage ------------
+runlog - opens a new runlog file to edit in your editor.
+runlog e filename - opens log entry with filename for editing. Must use full filename.
+runlog v filename - opens log entry with filename for viewing. Must use full filename.
+runlog d filename - delete runlog file with filename.
+runlog r - reads all entries (cats all files in the dir, pipes to less)
+runlog r yyyymmdd - reads entries from date yyyymmdd. One can specify just yyyymm, or just yyyy, even.
+runlog l - lists all runlog entries. Like r, it can be narrowed down with date parameters.
+runlog s searchterm - searches for searchterm in run entries.
+runlog mo yyyymm - gives a monthly report for month yyyymm 
+runlog h - displays this help message.
+DATES: YYYYMMDD means 4 digit year, 2 digit month, 2 digit day.
+This month is $thismonth, Today is $tday.
+-------------------------------------
+runlog is released according to GPL v. 3"
+
+else
+if [[ $1 = l ]]; then
+	if [[ $2 ]]; then
+		ls -1t | grep $2
+	else
+		ls -1t
+	fi
+else
+if [[ $1 = d ]]; then
+	read -p "Are you certain you wish to delete $2? " dr
+	if [[ $dr = y ]]; then
+		rm $2
+	else
+		exit
+	fi
+else
+if [[ $1 = v ]]; then
+	less $2
+else
+if [[ $1 = e ]]; then
+	$editor $2
+else
+if [[ $1 = s ]]; then
+	grep -iw $2 *
+else
+if [[ $1 = mo ]]; then
+	if [ -e $2.month ]; then
+		read -p "Report exists. View or Recreate? (v/r)" po0p
+		if [ $po0p = v ] ; then 
+			cat $2.month
+			exit
+		else
+			mv $2.month $2.month.bak
+		fi
+	fi
+		ttime=0
+		tdist=0
+		for di in $(ls $2*.run); do
+		grep Distance $di | awk '{ print $2 }' >> $2.distance
+		done
+		for dis in $(cat $2.distance); do
+		tdist=`echo "$tdist+$dis" | bc`
+		done
+		for ti in $(ls $2*.run); do
+		grep Time $ti | awk '{ print $2 }' >> $2.time
+		done
+		for tim in $(cat $2.time); do
+		ttime=`echo "$ttime+$tim" | bc`
+		done
+		avp=`echo "$ttime / $tdist" | bc`
+		echo "---- Monthly Run Report $2 ----" > $2.month
+		echo "Total Distance = $tdist
+Total Time = $ttime
+Average Pace = $avp mins/mile" >> $2.month
+		rm $2.distance
+		rm $2.time
+		cat $2.month
+		exit
+else
+	date=`date`
+	read -p "Distance (miles):  " dist
+	read -p "Time (minutes): " mins
+	read -p "Notes: " notes
+	mpm=`echo "$mins / $dist" | bc -l`
+	echo -e "\n$date\n\n$dist miles in $mins minutes, \nPace: $mpm min/mile\n\n-------------------\n\n$notes\n\n------------------\n" > $filedate.run
+	$editor $filedate.run
+	read -p "Post to my free-haven? (y/n) " post
+	if [[ $post = y ]]; then
+		echo -e "\n@runner #running\nposted with runlog - http://tonyb.us/runlog\n----------------\n" >> ~/.runlog/$filedate.run
+		ud="$(cat ~/.runlog/$filedate.run)"
+		title="Tony's Runlog"
+		echo "would you like to crosspost to "
+		read -p "statusnet? " snet
+		read -p "twitter? " twit
+		read -p "facebook? " fb
+		read -p "dreamwidth?  " dw
+		read -p "livejournal? " lj
+		read -p "insanejournal?" ij
+		read -p "tumblr? " tum
+		read -p "posterous? " pos
+		read -p "wordpress? " wp 
+		read -p "libertree? " lt
+		if [[ $(curl --ssl -u $fuser:$fpass -d "status=$ud&title=$title&ljpost_enable=$lj&ijpost_enable=$ij&posterous_enable=$pos&dwpost_enable=$dw&wppost_enable=$wp&tumblr_enable=$tum&facebook_enable=$fb&twitter_enable=$twit&libertree_enable=$lt:65&statusnet_enable=$snet&source=runlog.sh" $fsite/api/statuses/update.xml | grep error) ]]; then
+			echo "Error!"
+			exit
+		else 
+			echo "Success!"
+		read -p "Shall we have a look in your a browser now? (y/n): " op
+			if [ $op = "y" ]; then
+			xdg-open $fsite/u/$fuser
+			fi
+		fi
+	fi
+fi
+fi
+fi
+fi
+fi
+fi
+fi
+fi
+exit
